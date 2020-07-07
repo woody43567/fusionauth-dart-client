ab14a0e76784b2d6d48aca3d1f7b7b288126e3f3
Daniel DeGroff <daniel@fusionauth.io>
2020-07-07 11:09:25 -0600

    Stub out new APIs in client libs, and stub out front end actions.

--- lib/src/FusionAuthClient.dart
+++ lib/src/FusionAuthClient.dart
@@ -258,6 +258,36 @@ class FusionAuthClient {
         .go();
   }
 
+  /// Creates a form.  You can optionally specify an Id for the form, if not provided one will be generated.
+  ///
+  /// @param {String} formId (Optional) The Id for the form. If not provided a secure random UUID will be generated.
+  /// @param {FormRequest} request The request object that contains all of the information used to create the form.
+  /// @returns {Promise<ClientResponse<FormResponse>>}
+  Future<ClientResponse<FormResponse, Errors>> createForm(String formId, FormRequest request) {
+    return _start<FormResponse, Errors>()
+        .withUri('/api/form')
+        .withUriSegment(formId)
+        .withJSONBody(request)
+        .withMethod('POST')
+        .withResponseHandler(defaultResponseHandlerBuilder((d) => FormResponse.fromJson(d)))
+        .go();
+  }
+
+  /// Creates a form field.  You can optionally specify an Id for the form, if not provided one will be generated.
+  ///
+  /// @param {String} fieldId (Optional) The Id for the form field. If not provided a secure random UUID will be generated.
+  /// @param {FormFieldRequest} request The request object that contains all of the information used to create the form field.
+  /// @returns {Promise<ClientResponse<FormFieldResponse>>}
+  Future<ClientResponse<FormFieldResponse, Errors>> createFormField(String fieldId, FormFieldRequest request) {
+    return _start<FormFieldResponse, Errors>()
+        .withUri('/api/form/field')
+        .withUriSegment(fieldId)
+        .withJSONBody(request)
+        .withMethod('POST')
+        .withResponseHandler(defaultResponseHandlerBuilder((d) => FormFieldResponse.fromJson(d)))
+        .go();
+  }
+
   /// Creates a group. You can optionally specify an Id for the group, if not provided one will be generated.
   ///
   /// @param {String} groupId (Optional) The Id for the group. If not provided a secure random UUID will be generated.
@@ -555,6 +585,30 @@ class FusionAuthClient {
     return _start<void, Errors>()
         .withUri('/api/email/template')
         .withUriSegment(emailTemplateId)
+        .withMethod('DELETE')
+        .go();
+  }
+
+  /// Deletes the form for the given Id.
+  ///
+  /// @param {String} formId The Id of the form to delete.
+  /// @returns {Promise<ClientResponse<void>>}
+  Future<ClientResponse<void, Errors>> deleteForm(String formId) {
+    return _start<void, Errors>()
+        .withUri('/api/form')
+        .withUriSegment(formId)
+        .withMethod('DELETE')
+        .go();
+  }
+
+  /// Deletes the form field for the given Id.
+  ///
+  /// @param {String} fieldId The Id of the form field to delete.
+  /// @returns {Promise<ClientResponse<void>>}
+  Future<ClientResponse<void, Errors>> deleteFormField(String fieldId) {
+    return _start<void, Errors>()
+        .withUri('/api/form/field')
+        .withUriSegment(fieldId)
         .withMethod('DELETE')
         .go();
   }
@@ -1733,6 +1787,54 @@ class FusionAuthClient {
         .go();
   }
 
+  /// Retrieves the form with the given Id.
+  ///
+  /// @param {String} formId The Id of the form.
+  /// @returns {Promise<ClientResponse<FormResponse>>}
+  Future<ClientResponse<FormResponse, void>> retrieveForm(String formId) {
+    return _start<FormResponse, void>()
+        .withUri('/api/form')
+        .withUriSegment(formId)
+        .withMethod('GET')
+        .withResponseHandler(defaultResponseHandlerBuilder((d) => FormResponse.fromJson(d)))
+        .go();
+  }
+
+  /// Retrieves the form field with the given Id.
+  ///
+  /// @param {String} fieldId The Id of the form field.
+  /// @returns {Promise<ClientResponse<FormFieldResponse>>}
+  Future<ClientResponse<FormFieldResponse, void>> retrieveFormField(String fieldId) {
+    return _start<FormFieldResponse, void>()
+        .withUri('/api/form/field')
+        .withUriSegment(fieldId)
+        .withMethod('GET')
+        .withResponseHandler(defaultResponseHandlerBuilder((d) => FormFieldResponse.fromJson(d)))
+        .go();
+  }
+
+  /// Retrieves all of the forms fields
+  ///
+  /// @returns {Promise<ClientResponse<FormFieldResponse>>}
+  Future<ClientResponse<FormFieldResponse, void>> retrieveFormFields() {
+    return _start<FormFieldResponse, void>()
+        .withUri('/api/form/field')
+        .withMethod('GET')
+        .withResponseHandler(defaultResponseHandlerBuilder((d) => FormFieldResponse.fromJson(d)))
+        .go();
+  }
+
+  /// Retrieves all of the forms.
+  ///
+  /// @returns {Promise<ClientResponse<FormResponse>>}
+  Future<ClientResponse<FormResponse, void>> retrieveForms() {
+    return _start<FormResponse, void>()
+        .withUri('/api/form')
+        .withMethod('GET')
+        .withResponseHandler(defaultResponseHandlerBuilder((d) => FormResponse.fromJson(d)))
+        .go();
+  }
+
   /// Retrieves the group for the given Id.
   ///
   /// @param {String} groupId The Id of the group.
@@ -2745,6 +2847,36 @@ class FusionAuthClient {
         .withJSONBody(request)
         .withMethod('PUT')
         .withResponseHandler(defaultResponseHandlerBuilder((d) => EmailTemplateResponse.fromJson(d)))
+        .go();
+  }
+
+  /// Updates the form with the given Id.
+  ///
+  /// @param {String} formId The Id of the form to update.
+  /// @param {FormRequest} request The request object that contains all of the new form information.
+  /// @returns {Promise<ClientResponse<FormResponse>>}
+  Future<ClientResponse<FormResponse, Errors>> updateForm(String formId, FormRequest request) {
+    return _start<FormResponse, Errors>()
+        .withUri('/api/form')
+        .withUriSegment(formId)
+        .withJSONBody(request)
+        .withMethod('PUT')
+        .withResponseHandler(defaultResponseHandlerBuilder((d) => FormResponse.fromJson(d)))
+        .go();
+  }
+
+  /// Updates the form field with the given Id.
+  ///
+  /// @param {String} fieldId The Id of the form field to update.
+  /// @param {FormFieldRequest} request The request object that contains all of the new form field information.
+  /// @returns {Promise<ClientResponse<FormFieldResponse>>}
+  Future<ClientResponse<FormFieldResponse, Errors>> updateFormField(String fieldId, FormFieldRequest request) {
+    return _start<FormFieldResponse, Errors>()
+        .withUri('/api/form/field')
+        .withUriSegment(fieldId)
+        .withJSONBody(request)
+        .withMethod('PUT')
+        .withResponseHandler(defaultResponseHandlerBuilder((d) => FormFieldResponse.fromJson(d)))
         .go();
   }
 
